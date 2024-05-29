6e2b6e933a9c8baeb14814fa2c14eabce1c86834
Joshua Mincer <mincej20@gmail.com>
2024-05-29 13:12:26 -0600

    Function for sinking the log

---
+++ drop/modules/helpers/log_sinker.R
@@ -0,0 +1,19 @@
+stream_arg <- cfg.get("stream_to_log")
+
+logSinker <- function(snakemake, log_file, stream_arg){
+    if(stream_arg == "yes"){
+        log <- file(log_file, open = "wt")
+
+        sink(log, type = "output")
+        sink(log, type = "message")
+        print(snakemake)
+    } else if (stream_arg == "tee"){
+        log <- file(log_file, open = "wt")
+
+        sink(log, type = "output", split = TRUE)
+        sink(log, type = "message", split = TRUE)
+        print(snakemake)
+    } else {
+        saveRDS(snakemake, log_file)
+    }
+}
