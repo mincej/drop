77584f6f2fb5ede69435e692156c7aba66a46870
Joshua Mincer <mincej20@gmail.com>
2024-05-30 15:52:25 -0600

    Removed log_script, log_code generalizable

--- drop/modules/helpers/log_funcs.py
+++ drop/modules/helpers/log_funcs.py
@@ -10,38 +10,20 @@ cfg = drop.config.DropConfig(wbuild.utils.Config(), projectDir)
 # Get the stream argument to prevent redundant parameters. 
 stream_info = f"""TO_LOG='{cfg.get("stream_to_log")}'"""
 
-#https://stackoverflow.com/questions/16461656/how-to-pass-array-as-an-argument-to-a-function-in-bash#:~:text=You%20cannot%20pass%20an%20array%2C%20you%20can%20only%20pass%20its%20elements%20(i.e.%20the%20expanded%20array).
-log_script = stream_info + """
+log_code = f"""
+{stream_info}
 
-    function logScript(){
-        SCRIPT=$1
-        ARGS=$2[@]
-        ARGS=("${!ARGS}")
-        LOG=$3
+function logCode(){{
+    LOG=$1
+    local FUNC=$2
 
-        if [[ "$TO_LOG" == "yes" ]]; then 
-            $SCRIPT ${ARGS[@]} > $LOG 2>&1
-        elif [[ "$TO_LOG" == "tee" ]]; then
-            $SCRIPT ${ARGS[@]} 2>&1 | tee --append $LOG
-        else
-            $SCRIPT ${ARGS[@]}
-        fi
-    }
-
-"""
-
-log_code = stream_info + """
-
-    function logFunc(){
-        FUNC=$1
-        LOG=$2
-        
-        if [[ "$TO_LOG" == "yes" ]]; then 
-            $FUNC > $LOG 2>&1
-        elif [[ "$TO_LOG" == "tee" ]]; then 
-            $FUNC 2>&1 | tee --append $LOG
-        else
-            $FUNC
-    }
+    if [[ "$TO_LOG" == "yes" ]]; then 
+        $FUNC > $LOG 2>&1
+    elif [[ "$TO_LOG" == "tee" ]]; then 
+        $FUNC 2>&1 | tee --append $LOG
+    else
+        $FUNC
+    fi
+}}
 
 """
