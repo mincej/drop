38be0a6147fdec85a3a3f1b22002bd558c24be89
Joshua Mincer <mincej20@gmail.com>
2024-05-30 12:36:22 -0600

    str function for snakemake variable - more readable

--- drop/modules/helpers/log_sinker.R
+++ drop/modules/helpers/log_sinker.R
@@ -4,13 +4,13 @@ logSinker <- function(snakemake, log_file, stream_arg){
         sink(log, type = "output")
         sink(log, type = "message")
         on.exit(sink())
-        print(snakemake)
+        str(snakemake)
 
     } else if (stream_arg == "tee"){
 
         # Output snakemake env without burdening terminal with redundancy. 
         log <- file(log_file, "wt")
-        writeLines(paste0(capture.output(print(snakemake)), "\n"), log)
+        writeLines(paste0(capture.output(str(snakemake)), "\n"), log)
 
         sink(log, type = "output", split = TRUE)
         sink(log, type = "message")
